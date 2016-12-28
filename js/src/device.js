c311f61f5f9a1cd91ae256d54e76031081e013a6
Martin Drapeau <martindrapeau@gmail.com>
2016-12-28 06:36:34 -0500

    mobile menu ui improvements

--- js/src/device.js
+++ js/src/device.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
+
+  _.extend(Backbone, {
+    WEBAPP: window.navigator.standalone,
+    COCOON: window.navigator.isCocoonJS || window.cordova || false,
+    MOBILE: "onorientationchange" in window || window.navigator.msMaxTouchPoints || window.navigator.isCocoonJS || window.cordova
+  });
 	
   if (window.Keyboard && typeof window.Keyboard.hideFormAccessoryBar == 'function') Keyboard.hideFormAccessoryBar(true);
+
+  if (Backbone.WEBAPP || Backbone.COCOON) $('body').addClass('standalone');
 
 });
