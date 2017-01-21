6ec50d9b2ba4cd64fbcbd6f41961705149be8566
Martin Drapeau <martindrapeau@gmail.com>
2017-01-21 04:24:55 -0500

    trying to get deviceready working on iOS

--- js/src/device.js
+++ js/src/device.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).on(window.cordova ? 'deviceready' : 'ready', function() {
 
   _.extend(Backbone, {
     WEBAPP: window.navigator.standalone,
-    COCOON: window.navigator.isCocoonJS || window.cordova || window.NativeStorage || false,
-    MOBILE: "onorientationchange" in window || window.navigator.msMaxTouchPoints || window.navigator.isCocoonJS || window.cordova
+    COCOON: window.cordova || false,
+    MOBILE: "onorientationchange" in window || window.navigator.msMaxTouchPoints || window.cordova
   });
 	
   if (window.Keyboard && typeof window.Keyboard.hideFormAccessoryBar == 'function') Keyboard.hideFormAccessoryBar(true);
--- js/src/main.js
+++ js/src/main.js
@@ -1,32 +1,23 @@
-$(document).ready(function() {
+$(document).on(window.cordova ? 'deviceready' : 'ready', function() {
+
+  // Set up local storage adaptors (until we have a real backend)
+  Backbone.SessionModel.prototype.sync = Backbone.SessionCollection.prototype.sync = new BackboneLocalStorage('sessions', {data: window._sessions}).sync;
+  Backbone.OrganizationModel.prototype.sync = Backbone.OrganizationCollection.prototype.sync = new BackboneLocalStorage('organizations', {data: window._organizations}).sync;
+  Backbone.MatchModel.prototype.sync = Backbone.MatchCollection.prototype.sync = new BackboneLocalStorage('matches', {data: window._matches}).sync;
+  Backbone.PlayerModel.prototype.sync = Backbone.PlayerCollection.prototype.sync = new BackboneLocalStorage('players', {data: window._players}).sync;
+  Backbone.CategoryModel.prototype.sync = Backbone.CategoryCollection.prototype.sync = new BackboneLocalStorage('categories', {data: window._categories}).sync;
+  Backbone.RoundModel.prototype.sync = Backbone.RoundCollection.prototype.sync = new BackboneLocalStorage('rounds', {data: window._rounds}).sync;
+  Backbone.ProgramModel.prototype.sync = Backbone.ProgramCollection.prototype.sync = new BackboneLocalStorage('programs', {data: window._programs}).sync;
+  Backbone.TennisAppState.prototype.sync = new BackboneLocalStorage('app').sync;
   
-  function start() {
-
-    // Set up local storage adaptors (until we have a real backend)
-    Backbone.SessionModel.prototype.sync = Backbone.SessionCollection.prototype.sync = new BackboneLocalStorage('sessions', {data: window._sessions}).sync;
-    Backbone.OrganizationModel.prototype.sync = Backbone.OrganizationCollection.prototype.sync = new BackboneLocalStorage('organizations', {data: window._organizations}).sync;
-    Backbone.MatchModel.prototype.sync = Backbone.MatchCollection.prototype.sync = new BackboneLocalStorage('matches', {data: window._matches}).sync;
-    Backbone.PlayerModel.prototype.sync = Backbone.PlayerCollection.prototype.sync = new BackboneLocalStorage('players', {data: window._players}).sync;
-    Backbone.CategoryModel.prototype.sync = Backbone.CategoryCollection.prototype.sync = new BackboneLocalStorage('categories', {data: window._categories}).sync;
-    Backbone.RoundModel.prototype.sync = Backbone.RoundCollection.prototype.sync = new BackboneLocalStorage('rounds', {data: window._rounds}).sync;
-    Backbone.ProgramModel.prototype.sync = Backbone.ProgramCollection.prototype.sync = new BackboneLocalStorage('programs', {data: window._programs}).sync;
-    Backbone.TennisAppState.prototype.sync = new BackboneLocalStorage('app').sync;
-    
-    // Fetch state model from local storage and then start the app
-    var model = new Backbone.TennisAppState();
-    model.fetch().always(function() {
-      console.log('start app');
-      window.app = new Backbone.TennisApp({
-        model: model,
-        el: $('body')
-      });
+  // Fetch state model from local storage and then start the app
+  var model = new Backbone.TennisAppState();
+  model.fetch().always(function() {
+    console.log('start app');
+    window.app = new Backbone.TennisApp({
+      model: model,
+      el: $('body')
     });
-  }
-
-  if (Backbone.COCOON) {
-    document.addEventListener("deviceready", start, false);
-  } else {
-    start();
-  }
+  });
   
 });
